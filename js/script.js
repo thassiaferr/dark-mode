b69e1849cf240964c88b9616ffe1df21d12c761d
thassiaferr <thassiaferr@gmail.com>
2022-07-06 20:03:00 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+loadTheme();
+
+const changeThemeBtn = document.querySelector("#change-theme");
+
+// Toggle dark mode
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
+
+// Load light or dark mode
+function loadTheme() {
+    const darkMode = localStorage.getItem("dark")
+
+    if(darkMode) {
+        toggleDarkMode();
+    }
+}
+
+changeThemeBtn.addEventListener("change", function () {
+    toggleDarkMode();
+
+    // Save or remove dark mode
+    localStorage.removeItem("dark");
+
+    if(document.body.classList.contains("dark")) {
+        localStorage.setItem("dark", 1);
+    }
+});
+
